e799d0cf1feb0a8d50a7a5f0624b7bb62afcc45a
Neil-Ni <tzuyangni@gmail.com>
2014-10-16 11:26:48 -0400

    added another server folder for future use

--- app/ViewModels/MainViewModel.ts
+++ app/ViewModels/MainViewModel.ts
@@ -16,46 +16,50 @@ class MainViewModel extends Fayde.MVVM.ViewModelBase {
 	load() {
 		this.tableApi.getAll()
 			.end((err, res) => {
-                for (var x=0; x < res.body.length; x++) {
-                    var table = new Table();
-                    table.id = res.body[x].id;
-                    table.name = res.body[x].name;
-                    this.tables.Add(table);
-                }
+				for (var x=0; x < res.body.length; x++) {
+					var table = new Table();
+					table.id = res.body[x].id;
+					table.name = res.body[x].name;
+					this.tables.Add(table);
+				}
 			});
 	}
 
-    SelectTableName(obj: any) {
-    	this.selectedTable = obj.parameter;
-    }
+	SelectTableName(obj: any) {
+		this.selectedTable = obj.parameter;
+	}
 
-    CreateTable() {
-        var table = new Table();
-        table.name = this.newTableName;
-    	this.tableApi.post(table)
-    		.end((err, res) => {
-    			if (!err) {
-    				this.newTableName = "";
-                    table.id = res.body.id;
-                    this.tables.Add(table);
-    			}
-    		})
-    }
+	AddClass() {
 
-    CancelTable() {
-    	this.newTableName = "";
-    }
+	}
 
-    DeleteTable() {
-    	if (!this.selectedTable || !this.selectedTable.id) return;
-    	this.tableApi.del(this.selectedTable.id)
-    		.end((err, res) => {
-                var index = this.tables.IndexOf(this.selectedTable);
-    			if (index > -1) {
-                    this.tables.RemoveAt(index);
-    			}
-    		})
-    }
+	CreateTable() {
+		var table = new Table();
+		table.name = this.newTableName;
+		this.tableApi.post(table)
+			.end((err, res) => {
+				if (!err) {
+					this.newTableName = "";
+					table.id = res.body.id;
+					this.tables.Add(table);
+				}
+			})
+	}
+
+	CancelTable() {
+		this.newTableName = "";
+	}
+
+	DeleteTable() {
+		if (!this.selectedTable || !this.selectedTable.id) return;
+		this.tableApi.del(this.selectedTable.id)
+			.end((err, res) => {
+				var index = this.tables.IndexOf(this.selectedTable);
+				if (index > -1) {
+					this.tables.RemoveAt(index);
+				}
+			})
+	}
 
 }
 Fayde.MVVM.NotifyProperties(MainViewModel, ["tables", "selectedTable", "newTableName"]);
---
+++ db_server/README.md
@@ -0,0 +1,3 @@
+# db_server
+
+a [Sails](http://sailsjs.org) application
